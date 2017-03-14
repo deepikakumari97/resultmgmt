13e433cf47aec9a9eaf6165490dc3ddd7c31f772
harshita97 <hrshita97@gmail.com>
2017-03-14 20:03:08 +0530

    final

--- RESULT-MGT/resultms/result/views.py
+++ RESULT-MGT/resultms/result/views.py
@@ -6,11 +6,14 @@ def home(request):
     context = {}
     if request.POST.get('rollno') is not None:
         try:
+            subject=Subject.objects.all()
             student = Student.objects.get(rollNo=request.POST.get('rollno').lower())
             print(student.name)
             print(student.rollNo)
             context = {
                 'Student': student,
+                'Subject' : subject,
+
                 'Results': Result.objects.filter(student=student)
             }
         except Student.DoesNotExist:
